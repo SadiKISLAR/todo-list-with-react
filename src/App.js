d3c99f151a5149acd754a0741342a7a7cabfce64
SadiKISLAR <sadi22kislar@gmail.com>
2022-10-19 15:21:00 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Todoform from './components/Todoform';
+import Todolist from './components/Todolist';
+
 
 function App() {
-  return (
+  const [close, setClose] = useState(true);
+  const[todo, setTodo] = useState("");
+  const[todos, setTodos]= useState([]); 
+  const[date, setDate] = useState("");
+  const handleClose=(e)=> {
+    e.preventDefault();
+    setClose((prev)=> (prev = !prev));
+  }
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    if(todo !== "" && date !== "") {
+      setTodos([{id: '${todo}- ${Date.now()}', todo, date }, ...todos]);
+      setTodo("");
+      setDate("");
+    }
+   }
+
+  const handleDelete = (id)=> {
+    const delTodo = todos.filter((to)=> to.id !== id);
+    setTodos([...delTodo]);
+  }
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>Todo List</h1>
+        <button className="btn-close" onClick={handleClose}> {close? "close" : "show"}</button>
+        { close && (<div>
+        <Todoform handleSubmit={handleSubmit} todo={todo} date={date} setTodo={setTodo} setDate={setDate}/>
+        <Todolist todos={todos} handleDelete={handleDelete}/> </div>)}
+        
+        </div>
     </div>
   );
-}
+
+};
 
 export default App;
---
+++ src/components/Todoform.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Todoform = ({handleSubmit, todo, setTodo, date, setDate}) => {
+  return (
+    <form className='todoForm' onSubmit={handleSubmit}>
+        <label htmlFor="">Task</label>
+        <input className='input1' type="text" value={todo} placeholder="add task" onChange={(e) => setTodo(e.target.value)}/>
+        <label htmlFor="">Date&Time</label>
+        <input className='input2' type="text" value={date} placeholder="add time" onChange={(e) => setDate(e.target.value)}/>
+        <button className='btn-go' type='submit'>GO</button>
+    </form>
+  )
+}
+
+export default Todoform
