4f1524ec93e33aaf324a14bda3e8d5392da56dcc
SadiKISLAR <sadi22kislar@gmail.com>
2022-10-19 18:01:04 +0300

    son ksım

--- src/App.js
+++ src/App.js
@@ -1,46 +1,49 @@
-import { useState } from 'react';
-import './App.css';
-import Todoform from './components/Todoform';
-import Todolist from './components/Todolist';
-
-
+import { useState } from "react";
+import "./App.css";
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
 function App() {
   const [close, setClose] = useState(true);
-  const[todo, setTodo] = useState("");
-  const[todos, setTodos]= useState([]); 
-  const[date, setDate] = useState("");
-  const handleClose=(e)=> {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [date, setDate] = useState("");
+  const handleClose = (e) => {
     e.preventDefault();
-    setClose((prev)=> (prev = !prev));
-  }
-  
+    setClose((prev) => (prev = !prev));
+  };
   const handleSubmit = (e) => {
     e.preventDefault();
-    
-    if(todo !== "" && date !== "") {
-      setTodos([{id: '${todo}- ${Date.now()}', todo, date }, ...todos]);
+    if (todo !== "" && date !== "") {
+      setTodos([{ id: `${todo}-${Date.now()}`, todo, date }, ...todos]);
       setTodo("");
       setDate("");
     }
-   }
-
-  const handleDelete = (id)=> {
-    const delTodo = todos.filter((to)=> to.id !== id);
+  };
+  const handleDelete = (id) => {
+    const delTodo = todos.filter((to) => to.id !== id);
     setTodos([...delTodo]);
-  }
-    return (
+  };
+  return (
     <div className="App">
       <div className="container">
-        <h1>Todo List</h1>
-        <button className="btn-close" onClick={handleClose}> {close? "close" : "show"}</button>
-        { close && (<div>
-        <Todoform handleSubmit={handleSubmit} todo={todo} date={date} setTodo={setTodo} setDate={setDate}/>
-        <Todolist todos={todos} handleDelete={handleDelete}/> </div>)}
-        
-        </div>
+        <h1>Todo List App</h1>
+        <button className="btn-close" onClick={handleClose}>
+          {close ? "close" : "show"}
+        </button>
+        {close && (
+          <div>
+            <TodoForm
+              handleSubmit={handleSubmit}
+              todo={todo}
+              date={date}
+              setTodo={setTodo}
+              setDate={setDate}
+            />
+            <TodoList todos={todos} handleDelete={handleDelete} />
+          </div>
+        )}
+      </div>
     </div>
   );
-
-};
-
+}
 export default App;
--- src/components/Todolist.jsx
+++ src/components/Todolist.jsx
@@ -1,21 +1,27 @@
-import React, { useState } from 'react'
+import { useState } from "react";
 
-const Todolist = ({todos, handleDelete}) => {
+const TodoList = ({ todos, handleDelete }) => {
   const [line, setLine] = useState(true);
   return (
     <ul className="allTodos">
-        {todos.map((t) => (
-       <li className="singleTodo">
-        <div className={line?"inlist": "inlist-line"} onClick={()=>setLine(!line)}>
-          <span className="todoText" key={t.id}>{t.todo}</span>  
-          <span className="todoDate">{t.date}</span>  
-        </div>
-        <button className='btn-delete' onClick={() => handleDelete(t.id)}>Delete</button>
+      {todos.map((t) => (
+        <li className="singleTodo">
+          <div
+            className={line ? "inlist" : "inlist-line"}
+            onClick={() => setLine(!line)}
+          >
+            <span className="todoText" key={t.id}>
+              {t.todo}
+            </span>
+            <span className="todoDate">{t.date}</span>
+          </div>
+          <button className="btn-delete" onClick={() => handleDelete(t.id)}>
+            Delete
+          </button>
         </li>
-        ))}
-   
+      ))}
     </ul>
   );
 };
 
-export default Todolist
+export default TodoList;
